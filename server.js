ee95da75979efe2baa3152fd515e1c62616d9939
manakr01 <rahmathullah.manakunnan@nyumc.org>
2016-12-15 14:21:25 +0530

    Trying out db connectivity

--- server.js
+++ server.js
@@ -40,11 +40,14 @@ router.use(function(req, res, next) {
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next(); // make sure we go to the next routes and don't stop here
 });
+router.get('/', function (req, res) {
+  res.send('hello')
+})
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
 app.use('/', router);
-app.use('/api/products', ProductsRouter);
+//app.use('/api/products', ProductsRouter);
 
 // START THE SERVER
 // =============================================================================
